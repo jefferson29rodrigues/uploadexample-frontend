1a8e09b58be43c987f9463e9884ac54b781ce2f7
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2022-01-03 10:38:51 -0300

    estilização da imagem de preview filelist

--- src/components/FileList/styles.js
+++ src/components/FileList/styles.js
@@ -40,5 +40,12 @@ export const FileInfo = styled.div`
 `;
 
 export const Preview = styled.div`
-
+    width: 36px;
+    height: 36px;
+    border-radius: 5px;
+    background-image: url(${props => props.src});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 50% 50%;
+    margin-right: 10px;
 `;
